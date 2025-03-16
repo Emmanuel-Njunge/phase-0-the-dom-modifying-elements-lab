1f9e41f833ee0cb78539f8f8b859fc7b783ad9b9
Emmanuel Njungé Ngugi <ngugiemmanuelnjunge@gmail.com>
2025-03-16 12:36:36 +0300

    Remove main element and add new header with text 'Emmanuel is the champion'

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+const element = document.querySelector("main#main");
+if (element) {
+  element.remove();
+}
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "Emmanuel is the champion";
+document.body.append(newHeader);
+
